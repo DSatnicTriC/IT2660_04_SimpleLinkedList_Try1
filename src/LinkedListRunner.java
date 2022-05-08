0777600ecfdd2aff299f9186d787c87ea221088e
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-07 20:37:40 -0400

    cleaned up the print so that it has a counter

--- src/LinkedListRunner.java
+++ src/LinkedListRunner.java
@@ -1,28 +1,32 @@
 public class LinkedListRunner {
 	public static void main(String[] args) {
 		var rootNode = new Node("first");
-		
-		var insertAtEndValues = new String[] {"a", "b", "c"};
+
+		var insertAtEndValues = new String[] { "a", "b", "c" };
 		for (int i = 0; i < insertAtEndValues.length; i++) {
 			rootNode.insertAtEnd(new Node(insertAtEndValues[i]));
 		}
-		
+
 		rootNode = rootNode.insertInPositionOfThisOne(new Node("new first"));
 		rootNode.getNext().setValue("started out first");
-		
+
 		printNodeTree(rootNode);
 	}
-	
+
 	private static void printNodeTree(Node node) {
-		printNodeInformation(node);
+		var counter = 0;
+		printNodeInformation(node, counter);
 		var nextNode = node.getNext();
 		while (nextNode != null) {
-			printNodeInformation(nextNode);
+			counter++;
+			printNodeInformation(nextNode, counter);
 			nextNode = nextNode.getNext();
 		}
 	}
-	
-	private static void printNodeInformation(Node node) {
-		System.out.println("Value: " + node.getValue() + " ... List size (this node + children): " + node.getListSizeFromHereOnward());
+
+	private static void printNodeInformation(Node node, int counter) {
+		var printOutput = "Position: " + counter + " ... List size (this node + children): "
+				+ node.getListSizeFromHereOnward() + " ... Value: " + node.getValue();
+		System.out.println(printOutput);
 	}
 }
