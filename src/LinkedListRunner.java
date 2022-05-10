2c0006e3138bdc0cf45decae58ac23b662534342
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-09 22:16:59 -0400

    first whack at insertAtPosition - not quite there

--- src/LinkedListRunner.java
+++ src/LinkedListRunner.java
@@ -21,6 +21,20 @@ public class LinkedListRunner {
 		rootNode.getNext().setValue("started out first");
 
 		printNodeTree(rootNode);
+		
+		System.out.println("Now inserting some nodes");
+		var nodeArray = new Node[] {
+			new Node("insertion #1"),
+			new Node("insertion #2"),
+			new Node("insertion #3")
+		};
+		var positionArray = new int[] { 30, 1, 3 };
+		
+		for (int i = 0; i < nodeArray.length; i++) {
+			insertAtPosition(rootNode, nodeArray[i], positionArray[i]);
+		}
+		
+		printNodeTree(rootNode);
 	}
 
 	private static void printNodeTree(Node node) {
@@ -38,5 +52,14 @@ public class LinkedListRunner {
 		var printOutput = "Position: " + counter + " ... List size (this node + children): "
 				+ node.getListSizeFromHereOnward() + " ... Value: " + node.getValue();
 		System.out.println(printOutput);
+	}
+	
+	private static void insertAtPosition(Node rootNode, Node insertionNode, int position) {
+		try {
+			System.out.println("Inserting node at position: " + position);
+			rootNode.insertAtPosition(insertionNode, position);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- src/Node.java
+++ src/Node.java
@@ -13,17 +13,17 @@ public class Node {
 	public void setValue(String value) {
 		this.value = value;
 	}
-
+	
 	public Node getNext() {
 		return next;
 	}
 	
 	public void insertAtEnd(Node node) {
 		var lastNode = this;
-		var nextNode = lastNode.getNext();
+		var nextNode = lastNode.next;
 		while (nextNode != null) {
 			lastNode = nextNode;
-			nextNode = lastNode.getNext();
+			nextNode = lastNode.next;
 		}
 		
 		lastNode.next = node;
@@ -34,6 +34,19 @@ public class Node {
 		return node;
 	}
 	
+	public void insertAtPosition(Node insertionNode, int position) throws Exception {
+		if (position < 0) {
+			throw new Exception("No negative positions!");
+		}
+		if (position == 0) {
+			throw new Exception("Use insertInPositionOfThisOne to not lose root node");
+		}
+		
+		var nodeBefore = this.getNodeAtPosition(position - 1);
+		insertionNode.next = nodeBefore.next;
+		nodeBefore.next = insertionNode;
+	}
+	
 	public int getListSizeFromHereOnward() {
 		return this.getNumberOfChildren() + 1;
 	}
@@ -41,12 +54,23 @@ public class Node {
 	private int getNumberOfChildren( ) {
 		int childCount = 0;
 		
-		var iterator = this.getNext();
+		var iterator = this.next;
 		while (iterator != null) {
 			childCount++;
-			iterator = iterator.getNext();
+			iterator = iterator.next;
 		}
 		
 		return childCount;
+	}
+	
+	private Node getNodeAtPosition(int position) throws Exception {
+		var iterator = this;
+		for (int i = 0; i < position; i++) {
+			iterator = this.next;
+			if (iterator == null) {
+				throw new Exception("List does not have that many elements");
+			}
+		}
+		return iterator;
 	}
 }
